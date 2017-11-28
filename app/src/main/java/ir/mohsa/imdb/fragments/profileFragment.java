f74972dc46b89830153d90cc8b825f7eb8f3bd6e
mohsa <msalahshoor@gmail.com>
2017-11-28 13:05:04 +0330

    minor changes before lunch 09-07

--- app/src/main/java/ir/mohsa/imdb/fragments/profileFragment.java
+++ app/src/main/java/ir/mohsa/imdb/fragments/profileFragment.java
@@ -3,18 +3,80 @@ package ir.mohsa.imdb.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import ir.mohsa.imdb.R;
+import ir.mohsa.imdb.list.EndlessAdapter;
 
 /**
  * dCreated by 3801261697 on 26/11/2017.
  */
 
 public class profileFragment extends Fragment{
+    class MoviesViewHolder extends RecyclerView.ViewHolder {
+
+        public MoviesViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    class ProfileViewHolder extends RecyclerView.ViewHolder {
+
+        public ProfileViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    class EndlessProfileAdapter extends EndlessAdapter<RecyclerView.ViewHolder> {
+
+        @Override
+        public int getDataItemsCount() {
+            return 0;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onDataCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onDataBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        protected int getLoadingViewType() {
+            return 0;
+        }
+
+        @Override
+        protected void loadMore(int position, LoadMoreCallback callback) {
+
+        }
+    }
+
+    LinearLayoutManager layoutManager;
+    RecyclerView recyclerView;
+    EndlessProfileAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        
+        // 01-define layout to inflate, یک ریسایکلر ویو تنها
+        View toReturn = inflater.inflate(R.layout.profile_recyclerview,container,false);
+        // 02-define recycler view, همون لی اوت بالایی این بار با آی دی
+        recyclerView = (RecyclerView) toReturn.findViewById(R.id.Profile_RecyclerView_toInflate);
+        // 03-define layout manager
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        // 04-define adapter
+        adapter = new EndlessProfileAdapter();
+        recyclerView.setAdapter(adapter);
+        // 05-return
+        return toReturn;
     }
 }
